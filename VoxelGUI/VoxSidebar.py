7f16ff765903e2e952a2d21de5a0d456493768c1
franzpx125 <franzpx125@gmail.com>
2017-12-10 23:44:23 +0100

    Added depth map tab

--- VoxelGUI/VoxSidebar.py
+++ VoxelGUI/VoxSidebar.py
@@ -9,10 +9,12 @@ from PyQt5.QtCore import Qt
 from VoxHDFViewer import VoxHDFViewer
 from VoxPreprocessingPanel import VoxPreprocessingPanel
 from VoxRefocusingPanel import VoxRefocusingPanel
+from VoxDepthMapPanel import VoxDepthMapPanel
 
 VOX_SIDEBAR_HDFVIEW = "Dataset Info"
 VOX_SIDEBAR_PREPROC = "Pre-processing"
 VOX_SIDEBAR_REFOCUS = "Refocusing"
+VOX_SIDEBAR_DEPTHMAP = "Depth Map"
 
 class VoxSidebar(QWidget):
 
@@ -32,12 +34,16 @@ class VoxSidebar(QWidget):
 		# Configure the refocusing tab:
 		self.refocusingTab = VoxRefocusingPanel()
 
+        # Configure the refocusing tab:
+		self.depthMapTab = VoxDepthMapPanel()
+
 		# Configure the whole widget:
 		self.toolbox.addItem(self.hdfViewerTab, VOX_SIDEBAR_HDFVIEW)
 		self.toolbox.addItem(self.preprocessingTab, VOX_SIDEBAR_PREPROC)
 		self.toolbox.addItem(self.refocusingTab, VOX_SIDEBAR_REFOCUS)
+		self.toolbox.addItem(self.depthMapTab, VOX_SIDEBAR_DEPTHMAP)
 
-        # Define layout:
+		# Define layout:
 		layout = QVBoxLayout()
 		layout.addWidget(self.toolbox)	
 		layout.setContentsMargins(10,10,2,10) #left,top,right,bottom
